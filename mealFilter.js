60c14b70f23a4d45dacb7b3951408ab3f2298099
NirabHossain <nirabamth@gmail.com>
2021-05-28 15:17:49 +0600

    Final

--- mealFilter.js
+++ mealFilter.js
@@ -2,24 +2,35 @@ function displayMeal(){
     const cards= document.getElementById('displayMeal');
     cards.innerHTML="";
     const formText=document.getElementById('formInput');
-    console.log(formText.value);
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${formText.value}`)
     .then(res=>res.json())
     .then(data=>{
         data.meals.forEach(meal => {
-            const name=meal.strMeal,img=meal[`strMealThumb`],id=meal.idMeal;
+            const name=meal.strMeal,img=meal[`strMealThumb`];
             const card=document.createElement('div');
             const elements=document.createElement('ul');
-
+            
+            for(let i=0;i<20;i++){
+                const element = document.createElement('li');
+                const inTxt = meal[`strIngredient${i+1}`];
+                if(inTxt){
+                    element.innerHTML=inTxt;
+                    elements.appendChild(element);                    
+                }
+            }
             card.className="card p-2 m-2";
             card.innerHTML=`
                 <img src="${img}" class="card-img-top">
                 <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                      <h6>${id}</h6>
+                    <h3 class="card-title">${name}</h3>
+                    <h5>Ingredients: </h5>
                 </div>
             `;
+            card.appendChild(elements);
             cards.append(card);
+            card.addEventListener('click',function(){
+                document.getElementById('displaySingle').innerHTML=card.innerHTML;
+            })
         });
     })
     formText.value="";
